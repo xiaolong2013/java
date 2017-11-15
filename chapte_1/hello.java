9abf67152cc70bea25e03ade7576609f91daca16
yangxiaolong <yangxiaolong@fanghuwang.com>
2017-11-15 21:30:48 +0800

    java 语法

--- chapte_1/hello.java
+++ chapte_1/hello.java
@@ -1,8 +1,22 @@
 
 public class hello{
-
-    public static void main(String[] args){
-    	System.out.println("hello 你好!");
-    }
        
+    public static void main(String[] args){
+
+    	System.out.println("hello ");
+
+         
+    	 double[] myList = new double[2];
+         myList[0] = 5.6;
+         myList[1] = 3.4;
+          
+         double total = 0; 
+         
+         for(int i=0;i<myList.length;i++){
+            total += myList[i];   
+         }
+
+         System.out.println("总和为： " + total);
+    }   
+
 }
